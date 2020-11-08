d47cc5c276be003551be1302e1419d2ee3feb50a
salihgunay <salihgunay@gmail.com>
2020-11-08 22:54:55 +0300

    README.md improved

--- example/client.py
+++ example/client.py
@@ -8,9 +8,9 @@ asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 async def call(items):
     ipc = IPC('ws://localhost:8765', mode="client")
     await ipc.connect()
-    echo = ipc.proxy  # Get class instance proxy
+    my_class = ipc.proxy  # Get class instance proxy
 
-    jobs = [echo.echo(1, 2, 3, me="hello", you=3) for _ in range(items)]
+    jobs = [my_class.echo(1, 2, 3, me="hello", you=3) for _ in range(items)]
     t = time.time()
     res = await asyncio.gather(*jobs)
     print(res[0])
@@ -18,9 +18,9 @@ async def call(items):
     print(f"it took {(time.time() -t):.2f} seconds to complete {items} requests.\n"
           f"One request took {(time.time()-t) / items * 1000:.4f} millisecond")
 
-    sum_ = await echo.sum(3.14, 2.7)
+    sum_ = await my_class.sum(3.14, 2.7)
     print(sum_)
-    print(await echo.async_echo(1, 2, 3, me="hello", you=3))
+    print(await my_class.async_echo(1, 2, 3, me="hello", you=3))
 
     await ipc.disconnect()
 
