efa3b320f4931f18b0c425702a0dde5990f61223
Salih Günay <salihgunay@gmail.com>
2022-03-23 18:27:12 +0300

    compression added

--- example/ipc_client.py
+++ example/ipc_client.py
@@ -1,61 +1,23 @@
 import asyncio
 from src.ipc import AsyncIpcClient
-import time
 import uvloop  # Optional
 asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
-max_sleep_time = 2 ** 32
-import random
-
-
-class Hello:
-    async def hello(self):
-        #print("I am called men")
-        return "f you"
-
 
 async def call():
-    ipc = AsyncIpcClient(klass=Hello(), resending=False)
+    ipc = AsyncIpcClient()
     await ipc.connect()
-    my_class = ipc.server  # Get class instance proxy
-    items = 10_000
-    while True:
-        try:
-            jobs = [my_class.echo(1, 2, 3, me="hello", you=3) for _ in range(items)]
-            t = time.time()
-            res = await asyncio.gather(*jobs)
-        except Exception as e:
-            print(e)
-        #print(f"it took {(time.time() -t):.2f} seconds to complete {items} requests.\n"
-        #      f"One request took {(time.time()-t) / items * 1000:.4f} millisecond")
-        #print(res[0])
-        #await asyncio.sleep(8)
+    server_class = ipc.proxy  # Get class instance proxy
+    res = await server_class.echo("Hello World!")
+    print(res)
+    res = await server_class.sum(3, 5)
+    print(res)
 
     await ipc.disconnect()
 
-
-async def call_2():
-    ipc = AsyncIpcClient(klass=Hello(), resending=False)
-    await ipc.connect()
-    my_class = ipc.server  # Get class instance proxy
-    items = 1
-    while True:
-        try:
-            jobs = [my_class.echo(1, 2, 3, me="hello", you=3) for _ in range(items)]
-            t = time.time()
-            res = await asyncio.gather(*jobs)
-        except Exception as e:
-            print(e)
-        #print(f"it took {(time.time() -t):.2f} seconds to complete {items} requests.\n"
-        #      f"One request took {(time.time()-t) / items * 1000:.4f} millisecond")
-        #print(res[0])
-        await asyncio.sleep(8)
-
-    await ipc.disconnect()
 
 try:
-    jobs = [call_2() for _ in range(100)]
-    asyncio.get_event_loop().run_until_complete(asyncio.gather(*jobs))
+    asyncio.get_event_loop().run_until_complete(call())
 except KeyboardInterrupt:
     print("Keyboard interrupt")
 
--- example/ipc_server.py
+++ example/ipc_server.py
@@ -3,52 +3,34 @@ import websockets
 from src.ipc import AsyncIpcServer, MAX_SIZE
 import uvloop  # Optional
 asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
-import time
 
 
 class MyClass:
+    _secret_value = "My Secret"
 
-    def echo(self, *args, **kwargs):
-        return args, kwargs
+    @staticmethod
+    def echo(sentence: str):
+        return sentence
 
-    async def async_echo(self, *args, **kwargs):
+    @staticmethod
+    async def async_echo(sentence: str):
         await asyncio.sleep(.5)
-        return args, kwargs
+        return sentence
 
     @staticmethod
     def sum(a: float, b: float) -> float:
         return a + b
 
+    def get_secret_value(self) -> str:
+        return self._secret_value
+
 
 my_class = MyClass()
 
 
-async def send_example(client):
-    items = 100
-    while True:
-        jobs = [client.hello() for _ in range(items)]
-        t = time.time()
-        res = await asyncio.gather(*jobs)
-        #print(f"it took {(time.time() - t):.2f} seconds to complete {items} requests.\n"
-        #      f"One request took {(time.time() - t) / items * 1000:.4f} millisecond")
-        #print(res[0])
-        await asyncio.sleep(2)
-    print("exited loop")
-
-
-counter = 0
-async def echo(websocket, path):
-    global counter
-    counter += 1
-    print(counter)
+async def echo(websocket, _):
     ipc_server = AsyncIpcServer(my_class, websocket)
-    #asyncio.ensure_future(send_example(ipc_server.client))
-    async def fuck():
-        await asyncio.sleep(3)
-        await ipc_server.disconnect()
-    #asyncio.ensure_future(fuck())
     await ipc_server.listen()
-    counter -= 1
 
 
 if __name__ == "__main__":
